77f26c645cd9ab0916a691c1a8d4d9b9d6771119
Leandro <leandromoreiravaz@gmail.com>
2024-01-09 06:33:37 -0300

    fixed imgs

---
+++ src/gatas.js
@@ -0,0 +1,53 @@
+export const gatas = [
+    {name: "Kiki", link: "/kiki"},
+    {name: "Tiska", link: "/tiska"},
+    {name: "Gaxinha", link: "/gaxinha"},
+    {name: "Tatatinha", link: "/tatatinha"},
+    {name: "Tatasminha", link: "/tatasminha"},
+]
+
+
+export const fotos = [
+    {kot: ['tatatinha', 'tatasminha'], path: 'multikots/2_2.jpg', shape: 'default', desc: 'Tatatinha olha para a câmera com cara de besta enquanto a Tatasminha se lava.'},
+    {kot: ['tatatinha', 'tatasminha'], path: 'multikots/2_tatatinha_e_tatasminha.jpg', shape: 'default', desc: 'Duas pequenas bolas de pelo em seu habitat natural.'},
+    {kot: ['kiki', 'tiska'], path: 'multikots/2_tiskakiki.jpg', shape: 'wide', desc: 'Kiki e Tiska aproveitando um ar fresco na sacada.'},
+    {kot: ['tatatinha', 'tatasminha', 'gaxinha'], path: 'multikots/3_1.jpg', shape: 'default', desc: 'Gaxinha observa suas queridas bolinhas de pelo ambulantes dormirem, não desgrudando seus gigantescos olhos das duas por um único segundo.'},
+    {kot: ['tatatinha', 'tatasminha', 'gaxinha'], path: 'multikots/3_2.jpg', shape: 'wide', desc: 'Gaxinha flagra suas pequenas companheiras sendo malcriadas. Correção necessária 💢'},
+    
+    {kot: ['gaxinha'],  path: 'gaxinha/gaxinha1.jpg', shape: 'wide', desc: 'Gaxinha olhando para a câmera, seus característicos olhos esbugalhados sendo bem aparentes.'},
+    {kot: ['gaxinha'], path: 'gaxinha/gaxinha2.jpg', shape: 'default', desc: 'Gaxinha encarando a câmera novamente, que gata mais fotogênica...'},
+    {kot: ['gaxinha'], path: 'gaxinha/gaxinha3.jpg', shape: 'default', desc: 'Eu não tenho palavras para descrever esta foto, é muito charme para uma gata só.'},
+    {kot: ['gaxinha'], path: 'gaxinha/gaxinha4.png', shape: 'wide', desc: 'Parece que ela está assustada, mas ela tem esta expressão facial 70% do tempo mesmo.'},
+    {kot: ['gaxinha'], path: 'gaxinha/gaxinha_angry.png', shape: 'wide', desc: 'Você fez algo que irritou a Gaxinha, agora sofra as consequências...'},
+    {kot: ['gaxinha'], path: 'gaxinha/gaxinha_bucinho.jpg', shape: 'wide', desc: 'Gatinha sonolenta, dormindo e mostrando seu bucinho.'},
+    {kot: ['gaxinha'], path: 'gaxinha/gaxinha_eyes.jpg', shape: 'default', desc: 'The 1000 yard stare'},
+    {kot: ['gaxinha'], path: 'gaxinha/gaxinha_eyes2.png', shape: 'default', desc: 'Olhos gigantes, nada mais a dizer.'},
+    {kot: ['gaxinha'], path: 'gaxinha/gaxinha_np.jpg', shape: 'default', desc: 'NAPOLEÃO'},
+    {kot: ['gaxinha'], path: 'gaxinha/gaxinhapet.jpg', shape: 'default', desc: 'Carinho na gaxinha'},
+
+    {kot: ['kiki'], path: 'kiki/kiki2.jpg', shape: 'tall', desc: 'Kiki com seus lindos olhos azuis.'},
+    {kot: ['kiki'], path: 'kiki/kiki_bucinho1.png', shape: 'tall', desc: 'Kiki pedindo um carinho na barriga, que gatinha mais linda.'},
+    {kot: ['kiki'], path: 'kiki/kiki_graca1.jpg', shape: 'wide', desc: 'Kiki fazendo graça, ela adora ficar rolando no chão pedindo por atenção.'},
+    {kot: ['kiki'], path: 'kiki/kiki_graca2.jpg', shape: 'wide', desc: 'Fazendo mais graça, quanto amor...'},
+    {kot: ['kiki'], path: 'kiki/kiki_pat.jpg', shape: 'default', desc: 'Carinho na Kiki, lindinha.'},
+    {kot: ['kiki'], path: 'kiki/kiki_wet.png', shape: 'default', desc: 'Kiki depois do banho, confesso que rachei o bico quando vi isto ao vivo.'},
+    {kot: ['kiki'], path: 'kiki/kiki_young.jpg', shape: 'wide', desc: 'Kiki quando era mais jovem.'},
+    {kot: ['kiki'], path: 'kiki/kiki_young_sleep.jpg', shape: 'default', desc: 'A mimir.'},
+
+
+    {kot: ['tatasminha'], path: 'tatasminha/tatasminha1.jpg', shape: 'default', desc: 'Tatasminha olhando bem no fundo dos seus olhos.'},
+    {kot: ['tatasminha'], path: 'tatasminha/tatasminha_eyes.png', shape: 'wide', desc: '...'},
+
+    {kot: ['tatatinha'], path: 'tatatinha/tatatinha1.jpg', shape: 'default', desc: 'Fofura.'},
+    {kot: ['tatatinha'], path: 'tatatinha/tatatinha2.jpg', shape: 'default', desc: 'Tatatinha malhando, gada demais.'},
+    {kot: ['tatatinha'], path: 'tatatinha/tatatinha3.jpg', shape: 'default', desc: 'Tatatinha...'},
+    {kot: ['tatatinha'], path: 'tatatinha/tatatinha4.jpg', shape: 'default', desc: 'Cute.'},
+
+    {kot: ['tiska'], path: 'tiska/tiska1.jpg', shape: 'tall', desc: 'Grumpy tiska.'},
+    {kot: ['tiska'], path: 'tiska/tiska2.jpg', shape: 'default', desc: 'Sitting tiska'},
+    {kot: ['tiska'], path: 'tiska/tiska3.jpg', shape: 'wide', desc: 'Tiska deitada, só de boa.'},
+    {kot: ['tiska'], path: 'tiska/tiska4.jpg', shape: 'tall', desc: 'Tiska esparramada.'},
+    {kot: ['tiska'], path: 'tiska/tiska_bucinho.jpg', shape: 'default', desc: 'Tiska rolando no chão.'},
+    {kot: ['tiska'], path: 'tiska/tiska_micro.jpg', shape: 'default', desc: 'Tiska porém recém nascida, fofa demais.'},
+    {kot: ['tiska'], path: 'tiska/tiska_soy.jpg', shape: 'default', desc: 'Soycat.'},
+]
---
+++ src/views/Home.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="intro">
+        <h1>Bem vindo! 🐱</h1>
+        <p>Este site reunirá diversas fotos das minhas gatas.<br>
+        Criei este projeto mais para servir como uma prática com desenvolvimento de sites
+        utilizando <a href="https://vuejs.org/">Vue.js</a>, mas nada lhe impede de utilizar este site
+        para ficar admirando fotos fofinhas de gatinhos. <strike>Afinal de contas, quem não gosta de gatinhos?</strike>
+        </p>
+        <h4>Temos fotos dos seguintes bichanos:</h4>
+        <ul>
+            <li v-for="g in gatas" class="gatas">
+                <p> {{ g.name }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+    import {gatas} from '../gatas.js';
+    import { RouterLink } from 'vue-router';
+</script>
+
+<style lang="scss" scoped>
+    .intro {
+        background-color: rgba(36, 36, 36, 0.4);
+        width: clamp(414px, 70%, 1000px);
+        margin: auto;
+        border: 2px solid rgba(36, 36, 36, 0.4);
+        padding: 1%;
+    }
+    .intro h1 {
+        text-align: center;
+        font-size: clamp(1.8rem, 3.5vw, 3rem);
+    }
+    .intro p > strike {
+        color: rgba(8, 8, 8, 0.45);
+        font-weight: 550;
+    }
+    .intro {
+        font-size: clamp(1rem, 1.1vw, 1.25rem);
+    }
+    .gatas li {
+        margin: 0;
+        padding: 0;
+    }
+    a {
+        color: #060c13;
+        font-weight: bold;
+        text-decoration: none;
+    }
+    a:hover {
+        text-decoration: underline;
+    }
+</style>
